118b5f7c2005f2286b9e2b0740e83cb39fec32b7
Patrick Letourneux <patrick.letourneux20@gmail.com>
2021-09-14 10:08:00 +0200

    good comments

--- assets/event1.js
+++ assets/event1.js
@@ -1,28 +1,14 @@
 // patrick 2021/09
+// help choose colors in html css js GUI
 
-// just help 
-var info = document.getElementById("information")
 
-function blocMove(evt) {
-    var i="Position dans l'écran : (screenX, screenY) = ";
-    i+="("+evt.screenX+", "+evt.screenY+")<br />";
-    i+="Position dans la page : (clientX, clientY) = ";
-    i+="("+evt.clientX+", "+evt.clientY+")<br />";
-    i+="Position dans le bloc : (offsetX, offsetY) = ";
-    i+="("+evt.offsetX+", "+evt.offsetY+")<br />";
-    i+="Déplacement depuis mousemove : (movementX, movementY) = ";
-    i+="("+evt.movementX+", "+evt.movementY+")";
-    info.innerHTML=i;
-}
-document.addEventListener("mousemove",blocMove)
-
+// show rgb values in resut zone
 function afficheRGB(number){
     var redValue = parseInt(document.getElementById("raquette_Red"+number).style.marginLeft); // recup margeleft
     var greenValue = parseInt(document.getElementById("raquette_Green"+number).style.marginLeft); // recup margeleft
     var blueValue = parseInt(document.getElementById("raquette_Blue"+number).style.marginLeft); // recup margeleft
     var textValue = "rgb("+redValue+","+ greenValue+","+ blueValue+")"
     console.log(textValue);
-    // TODO reste a faire une regle de 3 par rapport a largeur bloc parent pour avoir des valeurs entre 0 et 255
 
     document.getElementById("resultColor"+number).style.backgroundColor = textValue;
     document.getElementById("resultColor"+number).innerText = textValue;
@@ -31,14 +17,13 @@ function afficheRGB(number){
     document.getElementById("raquette_Blue"+number).innerText = blueValue;
 }
 
-
 // class raquette  
 //raquette can move in X direction in his window width (bloc do not has specified width) with mouse down in the raquette
 // move can continu if mouse is still down an outside the raquette
 // not work if at the beginning raquette is not inside the bloc
 // raquette color change when move is active
-// not work if parent has border, need improvement
 // some bugs if user clic on the text inside raquette, need improvement
+// margin left value is used to move de raquette and also used for rgb value
 class Raquette {
     constructor(element,name,number){
         element.setAttribute("data-activeMove","nottomove"); // attribut needed to initiate value
@@ -94,9 +79,7 @@ raquette_9 = new Raquette(document.getElementById("raquette_Green3"),document.ge
 raquette_10 = new Raquette(document.getElementById("raquette_Blue3"),document.getElementById("raquette_Blue3").id,3);
 
 
-// console.log(document.querySelector("#raquette_1").style);
-// document.querySelector("#raquette_1").style.margin = "0px";
-// console.log(document.getElementById("raquette_1").getBoundingClientRect().width) ;
+
 
 
 
